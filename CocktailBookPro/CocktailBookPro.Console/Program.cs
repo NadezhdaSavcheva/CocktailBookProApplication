bc124976de4755ea0d994573d33e2fadf9fae4bf
anna-2323 <79278381+anna-2323@users.noreply.github.com>
2021-03-21 05:18:52 +0200

    Add Console View

---
+++ CocktailBookPro/CocktailBookPro.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CocktailBookPro.Services;
+using CocktailBookPro.Services.DAO;
+using CocktailBookPro.Business.Controllers;
+using CocktailBookPro.ConsoleApp.Menus;
+
+namespace CocktailBookPro.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CocktailBookProDBContext context = new CocktailBookProDBContext();
+
+            RecipeDAO recipeDAO = new RecipeDAO(context);
+            UserDAO userDAO = new UserDAO(context);
+            HomeDAO homeDAO = new HomeDAO(context);
+
+            UserController userController = new UserController(userDAO);
+            RecipeController recipeController = new RecipeController(recipeDAO, userDAO);
+            HomeController homeController = new HomeController(homeDAO);
+
+            LoginMenu login = new LoginMenu(userController, recipeController, homeController);
+        }
+    }
+}
