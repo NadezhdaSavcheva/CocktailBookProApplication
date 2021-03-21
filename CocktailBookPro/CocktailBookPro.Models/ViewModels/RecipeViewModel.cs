309a50c603041407ab3a78b7b4ba1e3adee44d78
anna-2323 <79278381+anna-2323@users.noreply.github.com>
2021-03-21 05:36:56 +0200

    Add model layer

---
+++ CocktailBookPro/CocktailBookPro.Models/ViewModels/RecipeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CocktailBookPro.Models.ViewModels
+{
+    public class RecipeViewModel
+    {
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public List<string> Ingredients { get; set; }
+        public List<string> Steps { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public List<string> Comments { get; set; }
+    }
+}
